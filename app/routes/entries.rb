d6e0b33a64ea58daca771eb90a9ceff5d1ea4d39
Owen Mockler <omockler@gmail.com>
2014-02-12 22:01:53 -0500

    Complete Entries

--- app/routes/entries.rb
+++ app/routes/entries.rb
@@ -2,26 +2,30 @@ module HarassForCash
   module Routes
     class Entries < Base
 
-      get '/entires' do
+      get '/entries' do
+        @entries = Hacker.find Event.current_raffle.entries
         slim :entries
       end
 
       get '/enter/:id' do |id|
-        # If code is not claimed, register a new hacker with that code
         hacker = get_hacker id
 
-        raffle = Event.current.raffles.first(drawn: false)
+        raffle = Event.current_raffle
         halt(500, "No Current Raffle") unless raffle
-        
-        Entry.create hacker: hacker, raffle: raffle
+
+        raffle.entries << hacker.id unless raffle.entries.include?(hacker.id)
+        raffle.save
+
+        flash[:success] = "#{hacker.name} was entered in this raffle."
         redirect '/entries'
       end
 
       private
 
         def get_hacker code
-          hacker = Hacker.first(qr: id)
-          redirect "/hackers/new/#{id}" unless hacker
+          # If code is not claimed, register a new hacker with that code
+          hacker = Hacker.first(qr: code)
+          redirect "/hackers/new/#{code}" unless hacker
           hacker
         end
     end
