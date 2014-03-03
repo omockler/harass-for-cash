818bed8c41c5f8661d8892c07ad3d9f8156e610f
Owen Mockler <omockler@gmail.com>
2014-03-02 22:06:34 -0500

    Fuck asset precompile

--- lib/tasks/assets.rb
+++ lib/tasks/assets.rb
@@ -1,19 +1,19 @@
-namespace :assets do
-  desc 'Precompile assets'
-  task :precompile => :app do
-    assets = HarassForCash::Routes::Base.assets
-    target = Pathname(HarassForCash::App.root) + 'public/assets'
+# namespace :assets do
+#   desc 'Precompile assets'
+#   task :precompile => :app do
+#     assets = HarassForCash::Routes::Base.assets
+#     target = Pathname(HarassForCash::App.root) + 'public/assets'
 
-    assets.each_logical_path do |logical_path|
-      if asset = assets.find_asset(logical_path)
-        filename = target.join(asset.digest_path)
-        FileUtils.mkpath(filename.dirname)
-        asset.write_to(filename)
+#     assets.each_logical_path do |logical_path|
+#       if asset = assets.find_asset(logical_path)
+#         filename = target.join(asset.digest_path)
+#         FileUtils.mkpath(filename.dirname)
+#         asset.write_to(filename)
 
-        filename = target.join(logical_path)
-        FileUtils.mkpath(filename.dirname)
-        asset.write_to(filename)
-      end
-    end
-  end
-end
+#         filename = target.join(logical_path)
+#         FileUtils.mkpath(filename.dirname)
+#         asset.write_to(filename)
+#       end
+#     end
+#   end
+# end
